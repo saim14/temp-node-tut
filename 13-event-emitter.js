6c1605bab45774bc279ff3ee5b3712d9f4fe3629
MD Saim Islam <saimislam@MDs-MacBook-Air.local>
2021-09-07 13:46:03 +0600

    All nodejs tutorial files

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -1,3 +1,13 @@
 const EventEmitter = require('events')
 
-const testV = 89;
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', (name, id) => {
+    console.log(`data recieved with user: ${name} and id: ${id}`)
+})
+customEmitter.on('response2', () => {
+    console.log(`some other logic here `)
+})
+
+customEmitter.emit('response', 'john', 34)
+customEmitter.emit('response2')
---
+++ 2-async-patterns/2-await-pattern.js
@@ -0,0 +1,39 @@
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile(
+      './content/result-mind-grenade2.txt',
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: 'a' }
+    )
+    console.log(first, second)
+    const third = await readFile('./content/result-mind-grenade2.txt', 'utf8')
+    console.log()
+    console.log(third)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
